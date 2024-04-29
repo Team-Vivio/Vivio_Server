4405468b201c094de505ebdabb9b43bfbcac729b
jainefer <jy5849@naver.com>
2024-04-29 11:53:29 +0900

    [feat] redis 건듬

--- src/main/java/vivio/spring/config/RedisConfig.java
+++ src/main/java/vivio/spring/config/RedisConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -19,5 +22,16 @@ public class RedisConfig {
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(host, port);
+    }
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        /* Java 기본 직렬화가 아닌 JSON 직렬화 설정 */
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return redisTemplate;
     }
 }
--- src/main/java/vivio/spring/util/RedisUtil.java
+++ src/main/java/vivio/spring/util/RedisUtil.java
@@ -13,20 +13,24 @@ import java.time.Duration;
 public class RedisUtil {
     private final StringRedisTemplate redisTemplate;//Redis에 접근하기 위한 Spring의 Redis 템플릿 클래스
 
-    public String getData(String key){//지정된 키(key)에 해당하는 데이터를 Redis에서 가져오는 메서드
-        ValueOperations<String,String> valueOperations=redisTemplate.opsForValue();
+    private final StringRedisTemplate template;
+
+    public String getData(String key) {
+        ValueOperations<String, String> valueOperations = template.opsForValue();
         return valueOperations.get(key);
     }
-    public void setData(String key,String value){//지정된 키(key)에 값을 저장하는 메서드
-        ValueOperations<String,String> valueOperations=redisTemplate.opsForValue();
-        valueOperations.set(key,value);
+
+    public boolean existData(String key) {
+        return Boolean.TRUE.equals(template.hasKey(key));
     }
-    public void setDataExpire(String key,String value,long duration){//지정된 키(key)에 값을 저장하고, 지정된 시간(duration) 후에 데이터가 만료되도록 설정하는 메서드
-        ValueOperations<String,String> valueOperations=redisTemplate.opsForValue();
-        Duration expireDuration= Duration.ofSeconds(duration);
-        valueOperations.set(key,value,expireDuration);
+
+    public void setDataExpire(String key, String value, long duration) {
+        ValueOperations<String, String> valueOperations = template.opsForValue();
+        Duration expireDuration = Duration.ofSeconds(duration);
+        valueOperations.set(key, value, expireDuration);
     }
-    public void deleteData(String key){//지정된 키(key)에 해당하는 데이터를 Redis에서 삭제하는 메서드
-        redisTemplate.delete(key);
+
+    public void deleteData(String key) {
+        template.delete(key);
     }
 }
