68c99756352de33a9bfc1ee2608737d34aef2532
jainefer <jy5849@naver.com>
2024-05-16 01:12:11 +0900

    [feat] 구글로그인 추가

--- src/main/java/vivio/spring/config/SecurityConfig.java
+++ src/main/java/vivio/spring/config/SecurityConfig.java
@@ -22,45 +22,43 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig  {
+public class SecurityConfig {
+
     private final ObjectMapper objectMapper;
-    @Value("{spring.security.oauth2.client.registraion.goolge.client-id}")
-    String clientId;
-    @Value("{spring.security.oauth2.client.registraion.goolge.client-id}")
-    String clientSecret;
+
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+    private String clientSecret;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeHttpRequests((authorizeRequests)->
+        http.authorizeHttpRequests(authorizeRequests ->
                 authorizeRequests.anyRequest().permitAll());
-        // 폼 로그인 방식 설정
-        http
-                .formLogin((auth) -> auth.loginPage("/oauth-login/login")
-                        .loginProcessingUrl("/oauth-login/loginProc")
-                        .usernameParameter("loginId")
-                        .passwordParameter("password")
-                        .defaultSuccessUrl("/oauth-login")
-                        .failureUrl("/oauth-login")
-                        .permitAll());
 
-        // OAuth 2.0 로그인 방식 설정
-        http
-                .oauth2Login((auth) -> auth.loginPage("/oauth-login/login")
-                        .defaultSuccessUrl("/oauth-login")
-                        .failureUrl("/oauth-login/login")
-                        .permitAll());
+        http.formLogin(auth -> auth.loginPage("/oauth-login/login")
+                .loginProcessingUrl("/oauth-login/loginProc")
+                .usernameParameter("loginId")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/oauth-login")
+                .failureUrl("/oauth-login")
+                .permitAll());
 
-        http
-                .logout((auth) -> auth
-                        .logoutUrl("/oauth-login/logout"));
+        http.oauth2Login(auth -> auth.loginPage("/oauth-login/login")
+                .defaultSuccessUrl("/oauth-login")
+                .failureUrl("/oauth-login/login")
+                .permitAll());
 
-        http
-                .csrf((auth) -> auth.disable());
+        http.logout(auth -> auth.logoutUrl("/oauth-login/logout"));
+
+        http.csrf(auth -> auth.disable());
 
         return http.build();
     }
